59dec5a94a7dd9c7a42a96c2380713c5196eda08
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2021-04-23 19:51:16 -0400

    Create vue.js boilerplate

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// const chess = new Chess()
+
+// while (!chess.game_over()) {
+//     const moves = chess.moves()
+//     const move = moves[Math.floor(Math.random() * moves.length)]
+//     chess.move(move)
+// }
+// console.log(chess.pgn())
---
+++ src/components/Users.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>Users:</h3>
+</template>
+<script>
+  export default {
+    name: 'Users',
+  }
+</script>
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
