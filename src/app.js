e8a0ef26f61dc7f500e5eb6d3d1dc7714586d59f
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2021-04-27 13:29:11 -0400

    Reset project

--- src/app.js
+++
@@ -1,8 +0,0 @@
-// const chess = new Chess()
-
-// while (!chess.game_over()) {
-//     const moves = chess.moves()
-//     const move = moves[Math.floor(Math.random() * moves.length)]
-//     chess.move(move)
-// }
-// console.log(chess.pgn())
