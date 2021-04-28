ca371461f8d8023e74982b611e91527e9fb1a646
Sharven Prasad Dhanasekar <sharven.dhanasekar@mail.utoronto.ca>
2021-04-28 15:08:50 -0400

    Implement castling

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="chessboard">
     <h1>Fog of war Chess!</h1>
-    <canvas id="board" width="600" height="600" class="board" 
+    <canvas id="board" width="680" height="680" class="board" 
       @mousemove="mouseMove"
       @mousedown="makeMove"></canvas>
-    <p class="status">{{status}}</p>    
+    <h1 class="status">{{status}}</h1>    
     <img src="../assets/pw.png" id="pw" style="display: none;">
     <img src="../assets/nw.png" id="nw" style="display: none;">
     <img src="../assets/bw.png" id="bw" style="display: none;">
@@ -48,13 +48,14 @@ export default {
       return this.chess.get(algebraic);
     },
 
-    makeBoard() {
+    renderBoard() {
       let ctx = this.canvas;
       var row, col;
       var whiteCol = "#e8ebef";
       var blackCol = "#7d8796";
       var selectedCol = "#fff06e";
       var legalMoveCol = "#0000ff";
+      var checkCol = "#ff0000";
       var alternate = true;
 
       // Draw squares
@@ -92,8 +93,9 @@ export default {
           // Check if player is in check
           if (this.chess.in_check()) {
             if (result['type'] == 'k' && result['color'] == this.chess.turn()) {
-              ctx.fillStyle = "#ff0000";
-              ctx.fillRect(col * this.sqWidth, row * this.sqWidth, this.sqWidth, this.sqWidth);
+              ctx.strokeStyle = checkCol;
+              ctx.lineWidth = 5;
+              ctx.strokeRect(col * this.sqWidth + 2.5, row * this.sqWidth + 2.5, this.sqWidth - 5, this.sqWidth - 5);
             }
           }
 
@@ -103,26 +105,53 @@ export default {
 
       // Draw legal moves
       if (this.selectedPiece) {
+        
         var i;
         for (i = 0; i < this.legalMoves.length; i++) {
-          var move = this.legalMoves[i];
 
+          var move = this.legalMoves[i];
           move = move.replace("+", "").replace("#", "");
 
-          var c = this.col_alg.indexOf(move[move.length - 2]);
-          var r = this.row_alg.indexOf(move[move.length - 1]);
-          
-          if (move.indexOf("=") != -1) {
-            // This is promotion move
-            c = this.col_alg.indexOf(move[0]);
-            r = this.row_alg.indexOf(move[1]);
+          if (move != "O-O" && move != "O-O-O") {
+
+            var c = this.col_alg.indexOf(move[move.length - 2]);
+            var r = this.row_alg.indexOf(move[move.length - 1]);
+            
+            if (move.indexOf("=") != -1) {
+              // This is promotion move
+              c = this.col_alg.indexOf(move[0]);
+              r = this.row_alg.indexOf(move[1]);
+            }
+            
+            ctx.strokeStyle = legalMoveCol;
+            ctx.beginPath();
+            ctx.lineWidth = 5;
+            ctx.arc(c * this.sqWidth + this.sqWidth / 2, r * this.sqWidth + this.sqWidth / 2, 10, 0, 2 * Math.PI);
+            ctx.stroke();
+
+          } else {
+
+            var specR, specC;
+            if (this.chess.turn() == 'w' && move == 'O-O') {
+              specR = 7;
+              specC = 6;
+            } else if (this.chess.turn() == 'w' && move == 'O-O-O') {
+              specR = 7;
+              specC = 2;
+            } else if (this.chess.turn() == 'b' && move == 'O-O') {
+              specR = 0;
+              specC = 6;
+            } else if (this.chess.turn() == 'b' && move == 'O-O-O') {
+              specR = 0;
+              specC = 2;
+            }
+
+            ctx.strokeStyle = legalMoveCol;
+            ctx.beginPath();
+            ctx.lineWidth = 5;
+            ctx.arc(specC * this.sqWidth + this.sqWidth / 2, specR * this.sqWidth + this.sqWidth / 2, 10, 0, 2 * Math.PI);
+            ctx.stroke();
           }
-          
-          ctx.strokeStyle = legalMoveCol;
-          ctx.beginPath();
-          ctx.lineWidth = 5;
-          ctx.arc(c * this.sqWidth + this.sqWidth / 2, r * this.sqWidth + this.sqWidth / 2, 10, 0, 2 * Math.PI);
-          ctx.stroke();
         }
       }
     },
@@ -133,22 +162,46 @@ export default {
       for (i = 0; i < this.legalMoves.length; i++) {
         var move = this.legalMoves[i];
         move = move.replace("+", "").replace("#", "");
-        var c = this.col_alg.indexOf(move[move.length - 2]);
-        var r = this.row_alg.indexOf(move[move.length - 1]);
-        if (move.indexOf("=") != -1) {
-          // This is promotion move
-          c = this.col_alg.indexOf(move[0]);
-          r = this.row_alg.indexOf(move[1]);
-        }
+        
+        if (move != 'O-O' && move != 'O-O-O') {
+          var c = this.col_alg.indexOf(move[move.length - 2]);
+          var r = this.row_alg.indexOf(move[move.length - 1]);
+          if (move.indexOf("=") != -1) {
+            // This is promotion move
+            c = this.col_alg.indexOf(move[0]);
+            r = this.row_alg.indexOf(move[1]);
+          }
 
-        if (c == this.col && r == this.row) {
-          playedMove = move;
-          break; 
+          if (c == this.col && r == this.row) {
+            playedMove = move;
+            break; 
+          }
+        } else {
+          var specR, specC;
+          if (this.chess.turn() == 'w' && move == 'O-O') {
+            specR = 7;
+            specC = 6;
+          } else if (this.chess.turn() == 'w' && move == 'O-O-O') {
+            specR = 7;
+            specC = 2;
+          } else if (this.chess.turn() == 'b' && move == 'O-O') {
+            specR = 0;
+            specC = 6;
+          } else if (this.chess.turn() == 'b' && move == 'O-O-O') {
+            specR = 0;
+            specC = 2;
+          }
+
+          if (specR == this.row && specC == this.col) {
+            playedMove = move;
+            break;
+          }
         }
       }
+      
       this.chess.move(playedMove);
       this.selectedPiece = null;
-      this.makeBoard();
+      this.renderBoard();
 
       if (this.chess.in_checkmate()) {
         this.status = "CHECKMATE!";
@@ -168,7 +221,7 @@ export default {
     mouseMove(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
-      this.makeBoard();
+      this.renderBoard();
     },
     
     makeMove(e) {
@@ -181,22 +234,24 @@ export default {
       }
 
       if (this.selectedPiece == null) {
+        // Select the piece
         this.selectedPiece = this.getPieceXY(this.x, this.y);
-  
         if (this.selectedPiece) {
           this.legalMoves = this.chess.moves({ square: this.col_alg[this.col] + this.row_alg[this.row] })
         }
       } else {
+        // Make the move
         this.movePiece();
       }
 
-      this.makeBoard();
+      this.renderBoard();
     }
   },
 
   mounted: function mounted() {
     var c = document.getElementById("board");
     this.canvas = c.getContext('2d');
+    
     const Chess = require('chess.js');
     // const chess = new Chess("8/6k1/6r1/3N4/1K6/8/2Q5/4R3 w - - 0 1");
     const chess = new Chess();
@@ -222,9 +277,9 @@ export default {
         this.pieceImages[col][pie] = img;
       }
     }
-    this.width = 600;
+    this.width = 680;
     this.sqWidth = Math.floor(this.width / 8);
-    this.makeBoard();
+    this.renderBoard();
   },
 }
 </script>
@@ -237,6 +292,5 @@ export default {
 }
 .status {
   font-weight: bold;
-  font-size: 20px;
 }
 </style>
